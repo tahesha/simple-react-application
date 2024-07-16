e72c51394572f07c2d811887ac18570ce65da815
Tahesha <taheshao@gmail.com>
2024-07-16 11:53:18 -0400

    Added functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // This will run on the first render but not on subsequent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
-
-export default App;
